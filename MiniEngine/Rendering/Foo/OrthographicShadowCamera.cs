ae4b423368138b8c297a5dc993cc7c7f821f0382
Roy Triesscheijn <roy-t@hotmail.com>
2018-04-15 12:21:36 +0200

    Add missing files

---
+++ MiniEngine/Rendering/Foo/OrthographicShadowCamera.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace MiniEngine.Rendering.Foo
+{
+    public class OrthographicShadowCamera : ShadowCamera
+    {
+        private float _minX;
+        private float _minY;
+        private float _maxX;
+        private float _maxY;
+
+        public float MinX
+        {
+            get { return _minX; }
+            set
+            {
+                _minX = value;
+                CreateProjection();
+            }
+        }
+
+        public float MinY
+        {
+            get { return _minY; }
+            set
+            {
+                _minY = value;
+                CreateProjection();
+            }
+        }
+
+        public float MaxX
+        {
+            get { return _maxX; }
+            set
+            {
+                _maxX = value;
+                CreateProjection();
+            }
+        }
+
+        public float MaxY
+        {
+            get { return _maxY; }
+            set
+            {
+                _maxY = value;
+                CreateProjection();
+            }
+        }
+
+        public OrthographicShadowCamera(
+            float minX,
+            float minY,
+            float maxX,
+            float maxY,
+            float nearZ,
+            float farZ)
+            : base(nearZ, farZ)
+        {
+            _minX = minX;
+            _minY = minY;
+            _maxX = maxX;
+            _maxY = maxY;
+
+            CreateProjection();
+        }
+
+        protected override void CreateProjection()
+        {
+            Projection = Matrix.CreateOrthographicOffCenter(_minX, _maxX, _minY, _maxY, NearZ, FarZ);
+        }
+    }
+}
+
---
+++ MiniEngine/Rendering/Foo/ShadowCamera.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace MiniEngine.Rendering.Foo
+{
+    public abstract class ShadowCamera : IViewPoint
+    {
+        private Matrix _projection;
+        private Matrix _world, _view, _viewProjection;
+        private Vector3 _position;
+        private float _nearZ;
+        private float _farZ;
+
+        public Matrix Projection
+        {
+            get { return _projection; }
+            set
+            {
+                _projection = value;
+                _viewProjection = _view * _projection;
+            }
+        }
+
+        public Matrix ViewProjection
+        {
+            get { return _viewProjection; }
+        }
+
+        public float NearZ
+        {
+            get { return _nearZ; }
+            set
+            {
+                _nearZ = value;
+                CreateProjection();
+            }
+        }
+
+        public float FarZ
+        {
+            get { return _farZ; }
+            set
+            {
+                _farZ = value;
+                CreateProjection();
+            }
+        }
+
+        public Matrix View
+        {
+            get { return _view; }
+        }
+
+        public Vector3 Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+                _world.Translation = value;
+                OnWorldMatrixChanged();
+            }
+        }
+
+        public Vector3 Up
+        {
+            get { return _world.Up; }
+        }
+
+        public Vector3 Down
+        {
+            get { return _world.Down; }
+        }
+
+        public Vector3 Left
+        {
+            get { return _world.Left; }
+        }
+
+        public Vector3 Right
+        {
+            get { return _world.Right; }
+        }
+
+        public Vector3 Forward
+        {
+            get { return _world.Forward; }
+        }
+
+        public Vector3 Backward
+        {
+            get { return _world.Backward; }
+        }
+
+        protected ShadowCamera(float nearZ, float farZ)
+        {
+            _nearZ = nearZ;
+            _farZ = farZ;
+
+            _world = Matrix.Identity;
+            _view = Matrix.Identity;
+        }
+
+        public void SetLookAt(Vector3 cameraPosition, Vector3 cameraTarget, Vector3 up)
+        {
+            _view = Matrix.CreateLookAt(cameraPosition, cameraTarget, up);
+            _world = Matrix.Invert(_view);
+            _position = cameraPosition;
+            OnWorldMatrixChanged();
+        }
+
+        public void SetOrientation(Quaternion orientation)
+        {
+            _world = Matrix.CreateFromQuaternion(orientation);
+            _world.Translation = _position;
+            OnWorldMatrixChanged();
+        }
+
+        private void OnWorldMatrixChanged()
+        {
+            _view = Matrix.Invert(_world);
+            _viewProjection = _view * _projection;
+        }
+
+        protected abstract void CreateProjection();
+    }
+}
