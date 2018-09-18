f381ab8957f89c6b44384978ebb00d1c91920350
Roy Triesscheijn <roy-t@hotmail.com>
2018-09-18 19:04:10 +0200

    Fix bounding rectangle bug with help from DexterZ

--- MiniEngine/Rendering/Primitives/BoundingRectangle.cs
+++ MiniEngine/Rendering/Primitives/BoundingRectangle.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
-using MiniEngine.Utilities.Extensions;
 
 namespace MiniEngine.Rendering.Primitives
 {
@@ -75,8 +74,8 @@ namespace MiniEngine.Rendering.Primitives
             var corners = box.GetCorners();
 
             foreach (var corner in corners)
-            {
-                var projectedCorner = Vector4.Transform(corner, matrix).ScaleToVector3();
+            {                
+                var projectedCorner = ToViewSpace(corner, matrix);
                 minX = Math.Min(minX, projectedCorner.X);                
                 maxX = Math.Max(maxX, projectedCorner.X);
 
@@ -85,6 +84,30 @@ namespace MiniEngine.Rendering.Primitives
             }
 
             return new BoundingRectangle(minX, maxX, minY, maxY);
+        }
+
+        private static Vector2 ToViewSpace(Vector3 worldPosition, Matrix viewProjection)
+        {
+            var x = (worldPosition.X * viewProjection.M11) + (worldPosition.Y * viewProjection.M21)
+                                                           + (worldPosition.Z * viewProjection.M31)
+                                                           + viewProjection.M41;
+
+            var y = (worldPosition.X * viewProjection.M12) + (worldPosition.Y * viewProjection.M22)
+                                                           + (worldPosition.Z * viewProjection.M32)
+                                                           + viewProjection.M42;
+
+            var w = (worldPosition.X * viewProjection.M14) + (worldPosition.Y * viewProjection.M24)
+                                                           + (worldPosition.Z * viewProjection.M34)
+                                                           + viewProjection.M44;
+
+            // w is negative when the camera has zoomed beyond the world position in which case
+            // it needs to be negated to be able to compute the correct screen position
+            w = Math.Abs(w);
+
+            x = x / w;
+            y = y / w;
+
+            return new Vector2(x, y);
         }
     }
 }
