fad49d8f15d1742a62d432af24635657a19fb885
Roy Triesscheijn <roy-t@hotmail.com>
2019-07-01 15:21:45 +0200

    Add slider for seed to blender

--- Scripts/primitives.py
+++ Scripts/primitives.py
@@ -3,7 +3,7 @@ import math
 import mathutils
 
 
-def createTetrahdron(context):
+def createTetrahdron():
     Vertices = \
         [
             mathutils.Vector((0, -1 / math.sqrt(3), 0)),
@@ -18,5 +18,22 @@ def createTetrahdron(context):
         [[0, 1, 2], [0, 1, 3], [1, 2, 3], [2, 0, 3]]
         )
     NewMesh.update()
-    NewObj = bpy.data.objects.new("Tetrahedron", NewMesh)
-    context.scene.objects.link(NewObj)
+    return bpy.data.objects.new("Tetrahedron", NewMesh)
+
+
+def createTurret(seed):
+    Vertices = \
+        [
+            mathutils.Vector((0, -1 / math.sqrt(3), 0)),
+            mathutils.Vector((0.5, 1 / (2 * math.sqrt(3)), 0)),
+            mathutils.Vector((-0.5, 1 / (2 * math.sqrt(3)), 0)),
+            mathutils.Vector((0, 0, math.sqrt(2 / 3))),
+        ]
+    NewMesh = bpy.data.meshes.new("Turret_Base")
+    NewMesh.from_pydata(
+        Vertices,
+        [],
+        [[0, 1, 2], [0, 1, 3], [1, 2, 3], [2, 0, 3]]
+        )
+    NewMesh.update()
+    return bpy.data.objects.new("Turret_Base", NewMesh)
--- Scripts/procedural_generation.py
+++ Scripts/procedural_generation.py
@@ -1,5 +1,6 @@
 import bpy
 import primitives
+# Inspired by: https://github.com/a1studmuffin/SpaceshipGenerator
 
 
 class ProceduralGenerationPanel(bpy.types.Panel):
@@ -10,9 +11,15 @@ class ProceduralGenerationPanel(bpy.types.Panel):
     bl_category = "Create"
     bl_label = "Procedural Generation"
 
+    bpy.types.Scene.seed = bpy.props.IntProperty(name="Seed")
+
     def draw(self, context):
-        TheCol = self.layout.column(align=True)
-        TheCol.operator("mesh.make_tetrahedron", text="Add Tetrahedron")
+        layout = self.layout
+        col = layout.column(align=True)
+        col.operator("mesh.make_tetrahedron", text="Add Tetrahedron")
+        col.operator("mesh.make_turret", text="Add Turret")        
+        
+        layout.prop(context.scene, "seed")
     # end draw
 
 # end ProceduralGenerationPanel
@@ -21,21 +28,49 @@ class ProceduralGenerationPanel(bpy.types.Panel):
 class MakeTetrahedron(bpy.types.Operator):
     bl_idname = "mesh.make_tetrahedron"
     bl_label = "Add Tetrahedron"
-    bl_options = {"UNDO"}    
+    bl_options = {"UNDO"}        
 
     def invoke(self, context, event):
-        primitives.createTetrahdron(context)        
+        mesh = primitives.createTetrahdron()
+        context.scene.objects.link(mesh)
         return {"FINISHED"}
     # end invoke
 
 # end MakeTetrahedron
 
 
+class MakeTurret(bpy.types.Operator):
+    bl_idname = "mesh.make_turret"
+    bl_label = "Add Tu2rret"
+    bl_options = {"UNDO"}
+
+    def invoke(self, context, event):
+        self.report({'INFO'}, "Building turret with seed: {0}"
+                    .format(context.scene.seed))
+        mesh = primitives.createTurret(context.scene.seed)
+        context.scene.objects.link(mesh)
+        return {"FINISHED"}
+    # end invoke
+
+# end MakeTetrahedron
+
+
+classes = {ProceduralGenerationPanel, MakeTetrahedron, MakeTurret}
+dependencies = {primitives}
+
+
 def register():
-    bpy.utils.register_class(MakeTetrahedron)
-    bpy.utils.register_class(ProceduralGenerationPanel)
+    for c in classes:
+        bpy.utils.register_class(c)
 
 
 def unregister():
-    bpy.utils.unregister_class(MakeTetrahedron)
-    bpy.utils.unregister_class(ProceduralGenerationPanel)
+    for c in classes:
+        bpy.utils.unregister_class(c)
+
+
+def reload_dependencies():
+    """Reload modules that this module depends on"""
+    import importlib
+    for d in dependencies:
+        importlib.reload(d)
