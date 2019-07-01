6a0f49ac848728fb7e924381184b051e16dceda7
Roy Triesscheijn <roy-t@hotmail.com>
2019-07-01 19:05:34 +0200

    Let script create two objects and a boolean modifier

--- Scripts/primitives.py
+++ Scripts/primitives.py
@@ -1,6 +1,17 @@
 import bpy
 import math
 import mathutils
+import random
+
+
+def applyBoolean(operation, name, a, b):
+    """UNION, DIFFERENCE, or INTERSECT"""
+    modifier = a.modifiers.new(type="BOOLEAN", name=name)
+    modifier.object = b
+    modifier.operation = operation
+    bpy.ops.object.modifier_apply(modifier=name, apply_as="DATA")
+
+    bpy.data.objects.remove(b)
 
 
 def createTetrahdron():
@@ -21,19 +32,37 @@ def createTetrahdron():
     return bpy.data.objects.new("Tetrahedron", NewMesh)
 
 
+def randomFloat(fMin, fMax):
+    return random.random() * (fMax - fMin) + fMin
+
+
 def createTurret(seed):
-    Vertices = \
-        [
-            mathutils.Vector((0, -1 / math.sqrt(3), 0)),
-            mathutils.Vector((0.5, 1 / (2 * math.sqrt(3)), 0)),
-            mathutils.Vector((-0.5, 1 / (2 * math.sqrt(3)), 0)),
-            mathutils.Vector((0, 0, math.sqrt(2 / 3))),
-        ]
-    NewMesh = bpy.data.meshes.new("Turret_Base")
-    NewMesh.from_pydata(
-        Vertices,
-        [],
-        [[0, 1, 2], [0, 1, 3], [1, 2, 3], [2, 0, 3]]
-        )
-    NewMesh.update()
-    return bpy.data.objects.new("Turret_Base", NewMesh)
+    random.seed(seed)
+
+    maxHeight = 10.0
+    minHeight = 6.0
+    minRadius = 1.0
+    maxRadius = 4.0
+
+    height = randomFloat(minHeight, maxHeight)    
+    radius = randomFloat(minRadius, maxRadius)
+    radiusBase = randomFloat(radius, radius + 2.0)
+
+    bpy.ops.mesh.primitive_cylinder_add(
+        radius=radiusBase,
+        location=(0, 0, 0)
+    )
+
+    base = bpy.data.objects['Cylinder']
+    base.name = "Base"
+
+    bpy.ops.mesh.primitive_cylinder_add(
+        radius=radius,
+        depth=height,
+        location=(0, 0, 0)
+    )
+
+    torso = bpy.data.objects['Cylinder']
+    torso.name = "Torso"
+
+    applyBoolean("DIFFERENCE", "diff_base_torso", torso, base)
--- Scripts/procedural_generation.py
+++ Scripts/procedural_generation.py
@@ -1,5 +1,6 @@
 import bpy
 import primitives
+import random
 # Inspired by: https://github.com/a1studmuffin/SpaceshipGenerator
 
 
@@ -15,20 +16,37 @@ class ProceduralGenerationPanel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
+
         col = layout.column(align=True)
+        col.label("Scripts:")
         col.operator("mesh.make_tetrahedron", text="Add Tetrahedron")
-        col.operator("mesh.make_turret", text="Add Turret")        
-        
-        layout.prop(context.scene, "seed")
+        col.operator("mesh.make_turret", text="Add Turret")
+
+        col2 = layout.column(align=True)
+        col2.label("Properties:")
+        col2.prop(context.scene, "seed")
+        col2.operator("editor.new_seed", text="New Seed")
     # end draw
 
 # end ProceduralGenerationPanel
 
 
+class NewSeed(bpy.types.Operator):
+    bl_idname = "editor.new_seed"
+    bl_label = "New Seed"
+
+    def invoke(self, context, event):
+        context.scene.seed = random.randint(0, pow(2, 31))
+        return {"FINISHED"}
+    # end invoke
+
+# end NewSeed
+
+
 class MakeTetrahedron(bpy.types.Operator):
     bl_idname = "mesh.make_tetrahedron"
     bl_label = "Add Tetrahedron"
-    bl_options = {"UNDO"}        
+    bl_options = {"UNDO"}
 
     def invoke(self, context, event):
         mesh = primitives.createTetrahdron()
@@ -41,21 +59,20 @@ class MakeTetrahedron(bpy.types.Operator):
 
 class MakeTurret(bpy.types.Operator):
     bl_idname = "mesh.make_turret"
-    bl_label = "Add Tu2rret"
+    bl_label = "Add Turret"
     bl_options = {"UNDO"}
 
     def invoke(self, context, event):
         self.report({'INFO'}, "Building turret with seed: {0}"
                     .format(context.scene.seed))
-        mesh = primitives.createTurret(context.scene.seed)
-        context.scene.objects.link(mesh)
+        primitives.createTurret(context.scene.seed)        
         return {"FINISHED"}
     # end invoke
 
 # end MakeTetrahedron
 
 
-classes = {ProceduralGenerationPanel, MakeTetrahedron, MakeTurret}
+classes = {ProceduralGenerationPanel, NewSeed, MakeTetrahedron, MakeTurret}
 dependencies = {primitives}
 
 
