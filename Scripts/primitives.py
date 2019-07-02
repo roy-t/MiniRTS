55bd86e6ad073ef616c1625abd9d59097d1e1208
Roy Triesscheijn <roy-t@hotmail.com>
2019-07-02 16:11:03 +0200

    Play with scale and extrusion

--- Scripts/primitives.py
+++ Scripts/primitives.py
@@ -1,7 +1,8 @@
 import bpy
+import bmesh
 import math
-import mathutils
 import random
+from mathutils import Vector, Matrix
 
 
 def setActive(activate):
@@ -9,7 +10,7 @@ def setActive(activate):
     activate.select = True
 
 
-def applyBoolean(operation, name, a, b):
+def applyBoolean(operation, name, a, b): 
     """For operation use either UNION, DIFFERENCE, or INTERSECT"""
     modifier = a.modifiers.new(type="BOOLEAN", name=name)
     modifier.object = b
@@ -27,13 +28,13 @@ def placeOnGroundPlane(model):
     model.location[2] = model.dimensions[2] / 2.0
 
 
-def createTetrahdron():
+def createTetrahdron(): 
     Vertices = \
         [
-            mathutils.Vector((0, -1 / math.sqrt(3), 0)),
-            mathutils.Vector((0.5, 1 / (2 * math.sqrt(3)), 0)),
-            mathutils.Vector((-0.5, 1 / (2 * math.sqrt(3)), 0)),
-            mathutils.Vector((0, 0, math.sqrt(2 / 3))),
+            Vector((0, -1 / math.sqrt(3), 0)),
+            Vector((0.5, 1 / (2 * math.sqrt(3)), 0)),
+            Vector((-0.5, 1 / (2 * math.sqrt(3)), 0)),
+            Vector((0, 0, math.sqrt(2 / 3))),
         ]
     NewMesh = bpy.data.meshes.new("Tetrahedron")
     NewMesh.from_pydata(
@@ -56,8 +57,8 @@ def randomRatio(value, minFactor, maxFactor):
 
 
 def createBody(prefix, builder):
-    minHeight = 3
-    maxHeight = 4
+    minHeight = 5
+    maxHeight = 7
     height = randomFloat(minHeight, maxHeight)
 
     minBotRadius = 2.25
@@ -111,6 +112,66 @@ def createBody(prefix, builder):
     return body
 
 
+def extrudeScale(body, faceIndex, extrusion, scale):
+    setActive(body)
+    bpy.ops.object.mode_set(mode='EDIT')      # Toggle edit mode
+    bpy.ops.mesh.select_mode(type='FACE')     # Change to face selection
+    bm = bmesh.from_edit_mesh(body.data)
+
+    # Select, and retrieve the face we need
+    bm.faces.ensure_lookup_table()
+    for f in bm.faces:
+        f.select = False
+
+    face = bm.faces[faceIndex]
+    face.select = True
+
+    # Perform the extrusion
+    bpy.ops.mesh.extrude_faces_move(
+        MESH_OT_extrude_faces_indiv={"mirror": False},
+        TRANSFORM_OT_shrink_fatten={
+            "value": extrusion,
+        }
+    )
+
+    # Rebuild face data
+    bm.faces.ensure_lookup_table()
+    for f in bm.faces:
+        f.select = False
+
+    face = bm.faces[faceIndex]
+
+    # Perform the scaling
+    c = face.calc_center_median()
+    T = Matrix.Translation(-c)
+    S = Matrix.Scale(scale, 4)
+    bmesh.ops.transform(bm, matrix=S, verts=face.verts, space=T)
+
+
+def createBlockBody(prefix, builder):
+    height = 0.2
+
+    minBotWidth = 1.0
+    maxBotWidth = 1.5
+    botWidth = randomFloat(minBotWidth, maxBotWidth)
+
+    builder.primitive_cube_add(
+        location=(0, 0, 0),
+    )
+
+    body = bpy.context.object
+    body.name = "{0}body".format(prefix)
+    body.scale[0] = botWidth
+    body.scale[1] = botWidth
+    body.scale[2] = height
+    
+    # Face 4 is the bottom face
+    # Face 5 is the top face
+
+    extrudeScale(body, 5, -3, 0.5)
+    return body
+
+
 def createNeck(prefix, builder, body):
     bodyRadius = min(body.dimensions[0], body.dimensions[1]) / 2.0
     neckRadius = randomRatio(bodyRadius, 0.5, 0.8)
@@ -136,17 +197,19 @@ def createNeck(prefix, builder, body):
     return neck
 
 
-def createTurret(prefix, seed):
+def createTurret(prefix, seed): 
     random.seed(seed)
     builder = bpy.ops.mesh
 
     # We build the turret from top to bottom, always placing the part
     # that was just build with its base on the ground plane
 
-    body = createBody(prefix, builder)
-    placeOnGroundPlane(body)
+    body = createBlockBody(prefix, builder)
+    # placeOnGroundPlane(body)
 
-    neck = createNeck(prefix, builder, body)
-    applyBoolean("UNION", "union_body_neck", body, neck)
+    # neck = createNeck(prefix, builder, body)
+    # applyBoolean("UNION", "union_body_neck", body, neck)
 
-    placeOnGroundPlane(body)
+    # placeOnGroundPlane(body)
+    
+    bpy.ops.object.mode_set(mode='OBJECT')
