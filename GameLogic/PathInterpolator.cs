1aaa81d4c3dad3661bd52b3ffbaefb8107ba5f11
Roy Triesscheijn <roy-t@hotmail.com>
2020-04-01 08:39:37 +0200

    Improve performance of line interpolator

--- GameLogic/PathInterpolator.cs
+++ GameLogic/PathInterpolator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 
 namespace MiniEngine.GameLogic
@@ -12,13 +13,11 @@ namespace MiniEngine.GameLogic
             if (path.WayPoints.Count > 2)
             {
                 var points = new List<Vector3>(path.WayPoints);
-                var start = points[0];
                 for (var r = 0; r < 5; r++)
                 {
                     points = InterpolateInternal(points);
                 }
 
-                points.Insert(0, start);
                 return new Path(points);
             }
 
@@ -36,22 +35,32 @@ namespace MiniEngine.GameLogic
                 var control = path[i - 1];
                 var end = path[i];
 
-                var a = Vector3.Lerp(start, control, l);
-                var b = Vector3.Lerp(start, control, l * 2);
-                var c = Vector3.Lerp(start, control, l * 3);
+                if (!AreOnOneLine(start, control, end))
+                {
+                    var a = Vector3.Lerp(start, control, l);
+                    var b = Vector3.Lerp(start, control, l * 2);
+                    var c = Vector3.Lerp(start, control, l * 3);
 
-                var a2 = Vector3.Lerp(control, end, l);
-                var b2 = Vector3.Lerp(control, end, l * 2);
-                var c2 = Vector3.Lerp(control, end, l * 3);
+                    var a2 = Vector3.Lerp(control, end, l);
+                    var b2 = Vector3.Lerp(control, end, l * 2);
+                    var c2 = Vector3.Lerp(control, end, l * 3);
 
-                points.Add(Vector3.Lerp(a, a2, l));
-                points.Add(Vector3.Lerp(b, b2, l * 2));
-                points.Add(Vector3.Lerp(c, c2, l * 3));
+                    points.Add(Vector3.Lerp(a, a2, l));
+                    points.Add(Vector3.Lerp(b, b2, l * 2));
+                    points.Add(Vector3.Lerp(c, c2, l * 3));
+                }
             }
 
 
             points.Add(path[path.Count - 1]);
             return points;
+        }
+
+        private static bool AreOnOneLine(Vector3 start, Vector3 control, Vector3 end)
+        {
+            var fullDistance = Vector3.Distance(start, end);
+            var viaDistance = Vector3.Distance(start, control) + Vector3.Distance(control, end);
+            return Math.Abs(fullDistance - viaDistance) < 0.001f;
         }
     }
 }
